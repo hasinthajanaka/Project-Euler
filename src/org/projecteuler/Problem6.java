d0ea14ec575857cb569d5dda739429dd4d649ca2
hasinthajanaka <hasinthajanaka@gmail.com>
2020-02-04 11:01:07 +0530

    Sum square difference
    Problem 6
    
    The sum of the squares of the first ten natural numbers is,
    
    12+22+...+102=385
    The square of the sum of the first ten natural numbers is,
    
    (1+2+...+10)2=552=3025
    Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025−385=2640.
    
    Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.

---
+++ src/org/projecteuler/Problem6.java
@@ -0,0 +1,31 @@
+package org.projecteuler;
+
+/*
+    Sum square difference
+    The sum of the squares of the first ten natural numbers is,
+
+    12+22+...+102=385
+    The square of the sum of the first ten natural numbers is,
+
+    (1+2+...+10)2=552=3025
+    Hence the difference between the sum of the squares of the first ten natural numbers and the square of
+    the sum is 3025−385=2640.
+
+    Find the difference between the sum of the squares of the first one hundred natural numbers and the square
+    of the sum.
+ */
+public class Problem6 {
+
+    public static void main(String[] args) {
+
+        double sumOfSquare = 0;
+        double squareOfSum = 0;
+
+        for (int i = 1; i <= 100; i++) {
+            sumOfSquare += Math.pow(i, 2);
+            squareOfSum += i;
+        }
+        double solution = Math.pow(squareOfSum, 2) - sumOfSquare;
+        System.out.println((long) solution);
+    }
+}
